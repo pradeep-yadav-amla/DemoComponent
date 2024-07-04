5324d7a5df42b0ef5f46f182d635e2394f3f14f0
Pradeep Yadav <pradeep.yadav@amla.io>
2024-07-04 16:25:00 +0530

    Update Paynow.js

--- Paynow.js
+++ Paynow.js
@@ -20,3 +20,7 @@
               </div>
           );
       };
+	  
+	        const domNode = document.getElementById('app')
+      const root = ReactDOM.createRoot(domNode);
+	   root.render(<App/>)
